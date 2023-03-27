5b85ec3ece95f9a1de2fdeee7a0a3e4990aa1651
Nick <124356718+NickJFry@users.noreply.github.com>
2023-03-27 15:11:28 +0300

    Brain_even update
    input in while

--- brain_games/brain_even.py
+++ brain_games/brain_even.py
@@ -1,7 +1,9 @@
 import random
 
-
-def is_even():
+count_answer = 3
+rules = 'Answer "yes" if the number is even, otherwise answer "no"'
+print(rules)
+while count_answer != 0:
     num = random.randint(1, 100)
     print('Question:', num)
     if num % 2 == 0:
@@ -9,21 +11,7 @@ def is_even():
     else:
         right_answer = 'no'
     user_answer = input('Your answer: ')
-    if user_answer == right_answer:
-        return right_answer, user_answer
-    else:
-        return user_answer, user_answer
-
-
-count_answer = 3
-i = 0
-while i < 1:
-    rules = 'Answer "yes" if the number is even, otherwise answer "no"'
-    print(rules)
-    i = i + 1
-while count_answer != 0:
-    even, not_even = is_even()
-    if even == not_even:
+    if right_answer == user_answer:
         print('Correct!')
         count_answer -= 1
     else:

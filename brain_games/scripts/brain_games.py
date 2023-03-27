d9f738c433eed6fe89e320acb7d40a06414937f3
Никита Бурбыга <nickjfry@MacBook-Air-Nick.local>
2023-03-27 17:56:58 +0300

    brain_games.py changed

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 from brain_games.cli import welcome_user
+from brain_games.game_even import is_even
 
 def greet():
 	print('Welcome to the Brain Games!')
 
 welcome_user()
+is_even()
 
 def main():
 	greet()

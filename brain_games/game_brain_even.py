fd80c35b7dfc4491947e6e7b9310ba24a66e6e00
Никита Бурбыга <nickjfry@MacBook-Air-Nick.local>
2023-03-28 19:20:33 +0300

    brain_games/game_brain_even.py updated

--- brain_games/game_brain_even.py
+++ brain_games/game_brain_even.py
@@ -2,26 +2,22 @@ import random
 
 
 def is_even():
-    num = random.randint(1, 100)
-    print('Question:', num)
-    if num % 2 == 0:
-        right_answer = 'yes'
+    count_answer = 3
+    rules = 'Answer "yes" if the number is even, otherwise answer "no"'
+    print(rules)
+    while count_answer != 0:
+        num = random.randint(1, 100)
+        print('Question:', num)
+        user_answer = input('Your answer: ')
+        if num % 2 == 0:
+            right_answer = 'yes'
+        else:
+            right_answer = 'no'
+        if right_answer == user_answer:
+            print('Correct!')
+            count_answer -= 1
+        else:
+            print('"yes" is wrong answer ;(. Correct answer was "no".\nLet\'s try again')
+            break
     else:
-        right_answer = 'no'
-    user_answer = input('Your answer: ')
-    return right_answer, user_answer
-
-
-count_answer = 3
-rules = 'Answer "yes" if the number is even, otherwise answer "no"'
-print(rules)
-while count_answer != 0:
-    even, not_even = is_even()
-    if even == not_even:
-        print('Correct!')
-        count_answer -= 1
-    else:
-        print('"yes" is wrong answer ;(. Correct answer was "no".\nLet\'s try again')
-        break
-else:
-    print('Congratulations!')
+        print('Congratulations!')

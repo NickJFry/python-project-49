2b29020721a7ca6bcaf5952220b32622c8c491ed
Никита Бурбыга <nickjfry@MacBook-Air-Nick.local>
2023-04-06 19:48:05 +0300

    brain even для записи из терминала

--- brain_games/Games/brain_even.py
+++ brain_games/Games/brain_even.py
@@ -5,23 +5,27 @@ rules = 'Answer "yes" if the number is even, otherwise answer "no"'
 
 
 def launch():
-    # user_name = prompt.string('May I have your name? ')
-    # print('Hello,', user_name, '!')
-    # tries = 3
-    # while tries != 0:
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
+    print('Hello,', user_name, '!')
+    print(rules)
+    tries = 3
+    while tries != 0:
         num = random.randint(1, 100)
         question = num
-        # print('Question:', question)
-        # user_answer = input('Your answer: ')
+        print('Question:', question)
+        user_answer = input('Your answer: ')
         if num % 2 == 0:
             right_answer = 'yes'
         else:
             right_answer = 'no'
-    #     if right_answer == user_answer:
-    #         print('Correct!')
-    #         tries -= 1
-    #     else:
-    #         print('"yes" is wrong answer ;(. Correct answer was "no".\nLet\'s try again')
-    #         break
-    # else:
-    #     print('Congratulations, 'f'{user_name}!')
+        if right_answer == user_answer:
+            print('Correct!')
+            tries -= 1
+        else:
+            print('"yes" is wrong answer ;(. Correct answer was "no".\nLet\'s try again')
+            break
+    else:
+        print('Congratulations, 'f'{user_name}!')
+
+launch()
